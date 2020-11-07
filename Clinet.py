e7b83f53d525af3e97e3fb0abf92d324bd853764
Danial <danial.kordmodanlou@gmail.com>
2020-11-07 22:24:32 +0330

    POST and GET methods implemented

--- Clinet.py
+++ Clinet.py
@@ -1,7 +1,11 @@
 import http.client
 conn = http.client.HTTPConnection("127.0.0.1:8000")
-conn.request("GET", "/")
+conn.request("POST", "<Whasup>")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
 
 
+if r1.status is 200:
+    while True:
+        str = input("enter message")
+        conn.send(str.encode())
--- Server.py
+++ Server.py
@@ -1,26 +1,44 @@
 import http.server
 import socketserver
+import re
 
-
+messeges=[]
 
 class KodeFunHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     # handle GET command
     def do_GET(self):
-        rootdir = 'c:/xampp/htdocs/'  # file location
         try:
             # send code 200 response
             self.send_response(200,"Hi")
+            self.send_header('Content-type', 'tex')
+            self.end_headers()
+            print("connection start")
+            return
 
+        except IOError:
+            self.send_error(404, 'file not found')
+
+
+    def do_POST(self):
+        try:
+            print("recieved " + self.requestline)
+            # send code 200 response
+            self.send_response(200,"Hi")
             # send header first
             self.send_header('Content-type', 'tex')
             self.end_headers()
 
+
+            messeges.append(re.search("<(.+?)>",self.requestline).group(1))
+            for messege  in messeges:
+                print(messege)
             print("wtf")
             return
 
         except IOError:
             self.send_error(404, 'file not found')
+
 
 
 def run():
@@ -29,6 +47,7 @@ def run():
     with socketserver.TCPServer(("127.0.0.1", 8000), Handler) as httpd:
         print("serving at port", 8000)
         httpd.serve_forever()
+
 
 
 if __name__ == '__main__':
